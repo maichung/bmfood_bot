216e9cec1a6d2eda2f60039e0a5163532397f12f
maichung <maichunglk@gmail.com>
2023-10-09 21:59:45 +0700

    Update bmfood.py

--- bmfood.py
+++ bmfood.py
@@ -6,6 +6,7 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Callback
 API_KEY = "6500460826:AAEhdBmolpAG81D96JRdbUvBBe4-WloYLk0"
 driver = ODBC Driver 17 for SQL Server
 server = '113.161.144.16'
+port = '1433'
 database = 'BMQuanLySanXuat'
 username = 'sa'
 password = 'BiNhMinHGroUpSQlSv2O18'
@@ -13,6 +14,7 @@ password = 'BiNhMinHGroUpSQlSv2O18'
 conn_str = (
     f'Driver={driver};'
     f'Server={server};'
+    f'Port={port};'
     f'Database={database};'
     f'UID={username};'
     f'PWD={password};'
